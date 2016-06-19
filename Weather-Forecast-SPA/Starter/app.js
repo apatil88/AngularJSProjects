0ec21304bc9bb45447226573800d872d7045494b
apatil88 <amrut.patil88@hotmail.com>
2016-06-19 13:54:02 -0400

    Add code to make API call to obtain weather data. Added some utility functions for date and temperature conversions.

--- Weather-Forecast-SPA/Starter/app.js
+++ Weather-Forecast-SPA/Starter/app.js
@@ -16,6 +16,14 @@ weatherApp.config(function($routeProvider) {
         templateUrl: 'pages/forecast.htm',
         controller: 'forecastController'
     })
+    
+    //To get weather for number of days in the request
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+    
+    
 });
 
 
@@ -37,11 +45,27 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
        cityService.city = $scope.city; 
     
     });
-
+    
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
     
     $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || 2;
+    
+    //API call to get weather data
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback : "JSON_CALLBACK"}, { get: {method: "JSONP"}});
+    
+    //Store the JSON response
+    $scope.weatherResult = $scope.weatherAPI.get( { q: $scope.city, cnt: $scope.days, APPID: 'c60596606b9c7eb8c6ac2dae9bf89f9c'});
+    
+    $scope.convertToFahrenheit = function(degK){
+        return Math.round((1.8 * (degK - 273)) + 32); 
+    };
+    
+    $scope.convertToDate = function(dt) {
+        return new Date(dt * 1000);  
+    };
 
 }]);
